07fb135976f89e8e8fb85738d6e1c919a50e5ae2
michel <michel.santos@sequor.com.br>
2025-04-22 19:21:50 -0300

    remoção de arquivo desnecessário

--- src/services/userAccountService.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API = import.meta.env.VITE_API_URL;
-
-export const getAllUserAccounts = async () => {
-  const response = await axios.get(`${API}/api/user-account/getAll`);
-  return response.data;
-};
